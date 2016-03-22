f12fe4a5434728bb9c968a527c66ce24af53f5cd
Samuele Colombo <samuele.colombo91@hotmail.it>
2016-03-22 17:55:48 +0100

    Added the stubs of BigInteger/ComplexMessage

---
+++ src/message/BigIntegerMessage.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Samuele Colombo
+ */
+public class BigIntegerMessage extends BaseMessage <BigInteger>
+{
+
+    @Override
+    public BigInteger getMessage() 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMessage(BigInteger t) 
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
