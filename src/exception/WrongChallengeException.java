780e74ee30911076ff237a21ce54ce1343cee597
Samuele Colombo <samuele.colombo91@hotmail.it>
2016-03-29 10:39:43 +0200

    Added new exception about wrong challenge

---
+++ src/exception/WrongChallengeException.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author Samuele Colombo
+ * @since 0.12
+ */
+public class WrongChallengeException extends Exception {
+
+    /**
+     * Creates a new instance of <code>WrongChallengeException</code> without
+     * detail message.
+     * @since 0.12
+     */
+    public WrongChallengeException() 
+    {
+        //
+    }
+
+    /**
+     * Constructs an instance of <code>WrongChallengeException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     * @since 0.12
+     */
+    public WrongChallengeException(String msg) 
+    {
+        super(msg);
+    }
+}
