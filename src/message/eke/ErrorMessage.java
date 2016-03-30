31cbb3355cd6ad74c00ce95c69f0dda1e8ad7181
Samuele Colombo <samuele.colombo91@hotmail.it>
2016-03-31 00:26:35 +0200

    Added ErrorMessage class

---
+++ src/message/eke/ErrorMessage.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message.eke;
+
+import message.StringMessage;
+
+/**
+ *
+ * @author Samuele Colombo
+ * @since 0.12
+ */
+public class ErrorMessage extends StringMessage
+{
+    /**
+     * 
+     * @param content 
+     * @since 0.12
+     */
+    public ErrorMessage(String content) 
+    {
+        super(content);
+    }
+    
+}
